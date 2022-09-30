bcca0c51b0d02349aa36e8b1f0a3a61fc55fd51e
MayureshGavai <blackoutmayuresh2912@gmail.com>
2022-10-01 00:58:00 +0530

    init

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const flash = require('connect-flash');
 
 
 const app = express();
-let port = process.env.PORT;
-if(port == null || port == ""){
-    port = 3000;
-}
+const port = process.env.PORT || 3000;
+
 
 require('dotenv').config();
 
