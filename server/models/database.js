6a46b781498cab60e7a229e1cd10774eb4554f30
MayureshGavai <blackoutmayuresh2912@gmail.com>
2022-10-01 13:57:46 +0530

    init

--- server/models/database.js
+++ server/models/database.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
